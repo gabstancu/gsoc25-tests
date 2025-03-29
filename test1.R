2e747b5848aaf6306452df178922320545f48c43
Gabriela Stancu <87585067+gabstancu@users.noreply.github.com>
2025-03-29 12:32:33 +0200

    ..

--- test1.R
+++ test1.R
@@ -9,7 +9,8 @@ visualize_sampling_2d <- function(n = 1000, step = 20) {
 
   quartz(title = "2d plot") # disable if not on macOS
   plot(samples[1, ], samples[2, ], col = "blue", pch = 20,
-       main = "2D",
+       cex = 0.6,
+       main = "",
        xlab = "X-axis", ylab = "Y-axis",
        xlim = c(-1, 1), ylim = c(-1, 1))
   abline(h = 0, v = 0, col = "black", lwd = 2)
@@ -26,11 +27,11 @@ visualize_sampling_3d <- function(n = 1000, step = 20) {
   open3d()
   plot3d(samples[1, ], samples[2, ], samples[3, ],
          col = "purple", size = 1.0, type = "s",
-         main = "3D", xlab = "x", ylab = "y", zlab = "z",
+         main = "", xlab = "x", ylab = "y", zlab = "z",
          xlim = c(-1, 1), ylim = c(-1, 1), zlim = c(-1, 1))
   print(rglwidget())
 }
 
 
-visualize_sampling_2d(n = 3000, step = 20)
-visualize_sampling_3d(n = 3000, step = 20)
+visualize_sampling_2d(n = 1500, step = 20)
+visualize_sampling_3d(n = 1500, step = 20)
